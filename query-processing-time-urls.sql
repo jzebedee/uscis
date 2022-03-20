83b54c966c7b6259e8c3ec747a7f3795360715ce
J. Zebedee <zebedee@code.gripe>
2022-03-20 16:44:54 -0500

    Add download processing times script

--- query-processing-time-urls.sql
+++ query-processing-time-urls.sql
@@ -2,4 +2,4 @@ WITH curl_args AS (
 	SELECT printf('"https://egov.uscis.gov/processing-times/api/processingtime/%s/%s" -o response-processing-time_%s_%s.json', name, json_each.value, name, json_each.value) AS args FROM forms, json_each(offices)
 )
 
-SELECT printf('curl -H "Referer: https://egov.uscis.gov/processing-times/" %s', group_concat(args,' ')) FROM curl_args;
+SELECT group_concat(args,' ') FROM curl_args;
