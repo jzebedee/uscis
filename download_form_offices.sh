631a681d7b40c660048b533f6b6ba8d699856858
J. Zebedee <zebedee@code.gripe>
2022-03-20 19:12:16 -0500

    Make cURL output quiet (with errors)

--- download_form_offices.sh
+++ download_form_offices.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 form=$1
 
-curl "https://egov.uscis.gov/processing-times/api/formoffices/$form" -H 'Referer: https://egov.uscis.gov/processing-times/'
+curl -s -S "https://egov.uscis.gov/processing-times/api/formoffices/$form" -H 'Referer: https://egov.uscis.gov/processing-times/'
