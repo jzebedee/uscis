209d3b3ba0ed62659b47d444b5b1a83d0afce65d
J. Zebedee <zebedee@code.gripe>
2022-05-05 14:48:23 -0500

    Include the form name in form_types table

--- populate_form_types.sh
+++ populate_form_types.sh
@@ -7,5 +7,5 @@ sqlite3 uscis.db "SELECT name FROM forms" | while read form; do
   json_file="response-form-types_$form.json"
   ./download_form_types.sh "$form" > "$json_file"
 
-  printf "$(cat populate-form-types-template.sql)" "$json_file" | sqlite3 uscis.db
+  printf "$(cat populate-form-types-template.sql)" "$json_file" "$form" | sqlite3 uscis.db
 done
