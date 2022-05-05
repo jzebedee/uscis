6b89b8fbe53b7f39dc74e90191c1e1ba12caa41c
J. Zebedee <zebedee@code.gripe>
2022-05-05 00:40:13 -0500

    Update form offices to form types

--- download_form_offices.sh
+++ download_form_offices.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 form=$1
 
-curl -s -S "https://egov.uscis.gov/processing-times/api/formoffices/$form" -H 'Referer: https://egov.uscis.gov/processing-times/'
+curl -s -S "https://egov.uscis.gov/processing-times/api/formtypes/$form" -H 'Referer: https://egov.uscis.gov/processing-times/'
