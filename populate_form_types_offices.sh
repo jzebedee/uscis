1b18d02d86cc9b6124eafc420b58970819ef878c
J. Zebedee <zebedee@code.gripe>
2022-06-26 05:15:13 -0500

    Remove unused vars in form types offices pop

--- populate_form_types_offices.sh
+++ populate_form_types_offices.sh
@@ -7,10 +7,6 @@ sqlite3 uscis.db < create-offices-table.sql
 
 for json_file in response-form-offices_*.json;
 do
-  form_chunk=$(echo $json_file | sed 's/.*_\(.*\)_\(.*\)\.json/\1|\2/')
-  form_name=$(echo $form_chunk | cut -d '|' -f 1)
-  form_key=$(echo $form_chunk | cut -d '|' -f 2)
-
 	# Fill out any offices we haven't seen yet in the offices table
   printf "$(cat populate-offices-table-template.sql)" "$json_file" | sqlite3 uscis.db
 
