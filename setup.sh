5c03ab0ade5b2a44bca36a742ef95858de2d3669
J. Zebedee <zebedee@code.gripe>
2022-09-27 10:10:09 -0500

    Explicitly pick the json build of sqlite

--- setup.sh
+++ setup.sh
@@ -2,8 +2,4 @@
 set -euxo pipefail
 
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-brew update && brew install sqlite3
-
-# make sure I'm not going insane
-sqlite3 --version
-which sqlite3
+brew update && brew install sqlite3 --with-json1
