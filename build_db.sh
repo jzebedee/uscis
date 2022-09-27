8c02f3ff7a9ab1419834c8b0605f95a7064c4fdc
J. Zebedee <zebedee@code.gripe>
2022-09-27 10:55:31 -0500

    Inline setup script

--- build_db.sh
+++ build_db.sh
@@ -2,7 +2,13 @@
 set -euxo pipefail
 
 # install dependencies
-./setup.sh
+# https://docs.brew.sh/Homebrew-on-Linux#install
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+
+brew update && brew install sqlite
 
 # forms
 ./populate_forms.sh
