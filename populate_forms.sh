c8fbeb1049104a08d7e382014c6373d6ba1e94ed
J. Zebedee <zebedee@code.gripe>
2022-09-27 10:32:50 -0500

    Go back to using bottle, but check sqlite3 version in subscript

--- populate_forms.sh
+++ populate_forms.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 curl -s -S 'https://egov.uscis.gov/processing-times/api/forms' -H 'Referer: https://egov.uscis.gov/processing-times/' -o response-forms.json
 
+which sqlite3
+
 sqlite3 uscis.db < create-forms-table.sql
 
 sqlite3 uscis.db < populate-forms-table.sql
--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-brew update && brew install sqlite -s
+brew update && brew install sqlite
