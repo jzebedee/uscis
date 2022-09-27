02e89ca805bfac62922dd3d1458bc46b9cbbe0d6
J. Zebedee <zebedee@code.gripe>
2022-09-27 10:42:50 -0500

    Screwball

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
+brew update && brew install sqlite
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-brew update && brew install sqlite
