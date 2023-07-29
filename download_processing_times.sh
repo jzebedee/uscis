179025a6adf45b69c114613a7bf82830374960b4
J. Zebedee <zebedee@code.gripe>
2023-07-29 23:11:51 +0000

    Remove hardcoded PEM

--- download_processing_times.sh
+++ download_processing_times.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-sqlite3 uscis.db < query-processing-time-urls.sql | xargs -t curl -s -S -H 'Referer: https://egov.uscis.gov/processing-times/' --cacert egov-uscis-gov.pem
+sqlite3 uscis.db < query-processing-time-urls.sql | xargs -t curl -s -S -H 'Referer: https://egov.uscis.gov/processing-times/'
--- populate_forms.sh
+++ populate_forms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-curl -s -S 'https://egov.uscis.gov/processing-times/api/forms' -H 'Referer: https://egov.uscis.gov/processing-times/' --cacert egov-uscis-gov.pem -o response-forms.json
+curl -s -S 'https://egov.uscis.gov/processing-times/api/forms' -H 'Referer: https://egov.uscis.gov/processing-times/' -o response-forms.json
 
 sqlite3 uscis.db < create-forms-table.sql
 
