94303a73390daac846b9f8d976dd3845016b7368
J. Zebedee <zebedee@code.gripe>
2023-07-29 23:11:51 +0000

    Remove hardcoded PEM

--- download_form_types_offices.sh
+++ download_form_types_offices.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-sqlite3 uscis.db < query-form-type-offices-urls.sql | xargs -t curl -s -S -H 'Referer: https://egov.uscis.gov/processing-times/' --cacert egov-uscis-gov.pem
+sqlite3 uscis.db < query-form-type-offices-urls.sql | xargs -t curl -s -S -H 'Referer: https://egov.uscis.gov/processing-times/'
