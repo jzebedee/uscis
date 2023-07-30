f088e1587afced0a2c15365e933e65ce6394a4dc
J. Zebedee <zebedee@code.gripe>
2023-07-30 03:00:24 +0000

    Make requests in parallel for processing time

--- download_processing_times.sh
+++ download_processing_times.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-sqlite3 uscis.db < query-processing-time-urls.sql | xargs -t curl -s -S -H 'Referer: https://egov.uscis.gov/processing-times/'
+sqlite3 uscis.db < query-processing-time-urls.sql | xargs -t curl -s -S -Z -H 'Referer: https://egov.uscis.gov/processing-times/'
