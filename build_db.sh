c7d3e5ed9b8d4ac3ddd7ff18966f4e147f7499f1
J. Zebedee <zebedee@code.gripe>
2025-02-03 06:16:10 +0000

    Download curlscript

--- build_db.sh
+++ build_db.sh
@@ -10,6 +10,7 @@ set -euxo pipefail
 # install curl-impersonate
 curl -sL https://github.com/lwthiker/curl-impersonate/releases/download/v0.6.1/curl-impersonate-v0.6.1.x86_64-linux-gnu.tar.gz | sudo tar -xz -C /usr/bin
 curl -sL https://github.com/lexiforest/curl-impersonate/releases/download/v0.9.1/curl-impersonate-v0.9.1.x86_64-linux-gnu.tar.gz | sudo tar -xz -C /usr/bin
+curl -sL "$CURLSCRIPT_URL" | sudo tar -xz -C /usr/bin
 
 # populate CF cookies
 ./populate_cookies.sh
