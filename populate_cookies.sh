c8e6c0de1bf6a805058cef0c970619076300cb31
J. Zebedee <zebedee@code.gripe>
2025-02-02 23:56:04 -0600

    Pass URL to stealth endpoint

--- populate_cookies.sh
+++ populate_cookies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
+URL="https://egov.uscis.gov/processing-times/"
+
 if [ -n "${STEALTH:-}" ]; then
   JSON_COOKIES="${TMPDIR:-/tmp}/uscis_cookies.json"
   COOKIE_JAR="uscis.cookies"
-  curl --fail-with-body -s -S "$STEALTH_ENDPOINT" > "$JSON_COOKIES"
+  curl --fail-with-body -s -S "${STEALTH_ENDPOINT}${URL}" > "$JSON_COOKIES"
   python3 convert_cookies.py "$JSON_COOKIES" > "$COOKIE_JAR"
 else
-  ./curl_request.sh "https://egov.uscis.gov/processing-times/"
+  ./curl_request.sh "$URL"
 fi
