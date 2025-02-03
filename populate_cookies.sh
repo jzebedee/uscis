e6834e478847e091186898f341036b82657ef521
J. Zebedee <zebedee@code.gripe>
2025-02-03 04:50:08 +0000

    Add stealth options to populate_cookies

--- populate_cookies.sh
+++ populate_cookies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-./curl_request.sh "https://egov.uscis.gov/processing-times/"
+if [ -n "${STEALTH:-}" ]; then
+  JSON_COOKIES="${TMPDIR:-/tmp}/uscis_cookies.json"
+  COOKIE_JAR="uscis.cookies"
+  curl --fail-with-body -s -S "$STEALTH_ENDPOINT" > "$JSON_COOKIES"
+  python3 convert_cookies.py "$JSON_COOKIES" > "$COOKIE_JAR"
+else
+  ./curl_request.sh "https://egov.uscis.gov/processing-times/"
+fi
