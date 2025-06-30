3d0a3e915c5b17211889f2ab136046b381a089b9
J. Zebedee <zebedee@code.gripe>
2025-06-30 14:50:55 +0000

    Set referer when collecting FOIA metrics

--- populate_foia_processing_times.sh
+++ populate_foia_processing_times.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-./curl_request.sh "https://first.uscis.gov/api/public/foia-metrics" -o foia-metrics.json
+REFERER_HEADER='Referer: https://first.uscis.gov/'
+
+./curl_request.sh "https://first.uscis.gov/api/public/foia-metrics" \
+  -o foia-metrics.json \
+  -H "$REFERER_HEADER"
 
 sqlite3 uscis.db < create-foia-processing-time-table.sql
 
