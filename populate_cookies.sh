c5126f62c75829c5e05d738ccb25e961314e6a9a
J. Zebedee <zebedee@code.gripe>
2025-06-30 14:55:41 +0000

    Update populate_cookies

--- populate_cookies.sh
+++ populate_cookies.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-URL="https://egov.uscis.gov/processing-times/"
+TARGET_URL="https://egov.uscis.gov/processing-times/"
 
 if [ -n "${STEALTH:-}" ]; then
   JSON_COOKIES="${TMPDIR:-/tmp}/uscis_cookies.json"
+  COOKIE_URL="${STEALTH_ENDPOINT}${TARGET_URL}"
+  curl --fail-with-body -s -S "${COOKIE_URL}" > "$JSON_COOKIES"
   COOKIE_JAR="uscis.cookies"
-  curl --fail-with-body -s -S "${STEALTH_ENDPOINT}${URL}" > "$JSON_COOKIES"
   python3 convert_cookies.py "$JSON_COOKIES" > "$COOKIE_JAR"
 else
-  ./curl_request.sh "$URL"
+  ./curl_request.sh "$TARGET_URL"
 fi
