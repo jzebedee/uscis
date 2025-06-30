5d641df6114d47357fd8a062e35dc1a698049b9e
J. Zebedee <zebedee@code.gripe>
2025-06-30 16:46:42 +0000

    Use curl_chrome138 in requests

--- curl_request.sh
+++ curl_request.sh
@@ -6,5 +6,5 @@ DEFAULTS=" --fail-with-body -s -S"
 COOKIE_JAR="uscis.cookies"
 COOKIE_DEFAULTS=" -b ${COOKIE_JAR} --cookie-jar ${COOKIE_JAR}"
 
-curl_chrome136 ${PARAMS} ${DEFAULTS} \
+curl_chrome138 ${PARAMS} ${DEFAULTS} \
   ${COOKIE_DEFAULTS}
