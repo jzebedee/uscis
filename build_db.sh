1da1425a6ad13731b50d38f08e4cf7aa267f288d
J. Zebedee <zebedee@code.gripe>
2025-06-30 16:46:30 +0000

    Break out curl setup to install_curl.sh

--- build_db.sh
+++ build_db.sh
@@ -19,14 +19,7 @@ command -v sqldiff > /dev/null
 command -v sqlite3 > /dev/null
 
 # install curl-impersonate
-CURL_URL="https://github.com/lexiforest/curl-impersonate/releases/download/v1.1.0/curl-impersonate-v1.1.0.x86_64-linux-gnu.tar.gz"
-CURL_SHA="69dc4835aea5241e85742e465f2ea70c27e4db02675ccd6f8ae5a63a919bc098"
-CURL_OUT="$(mktemp)"
-
-curl -sL -o "$CURL_OUT" "$CURL_URL"
-echo "$CURL_SHA  $CURL_OUT" | sha256sum -c
-sudo tar -xz -C /usr/bin -f "$CURL_OUT"
-rm "$CURL_OUT"
+./install_curl.sh
 
 # populate CF cookies
 ./populate_cookies.sh
