001eb84f5048a1a5d1b02093b9a13576d3096677
J. Zebedee <zebedee@code.gripe>
2025-07-01 06:22:43 +0000

    Roll to 138

--- populate_cookies.sh
+++ populate_cookies.sh
@@ -6,7 +6,7 @@ TARGET_URL="$1"
 if [ -n "${STEALTH:-}" ]; then
   JSON_COOKIES="$(mktemp)"
   COOKIE_URL="${STEALTH_ENDPOINT}${TARGET_URL}"
-  ./curl_request.sh "${COOKIE_URL}" > "$JSON_COOKIES"
+  curl --fail-with-body -s -S "${COOKIE_URL}" > "$JSON_COOKIES"
   COOKIE_JAR="uscis.cookies"
   python3 convert_cookies.py "$JSON_COOKIES" > "$COOKIE_JAR"
 else
