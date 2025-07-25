88139e6d69b2694cc23ae539ab8beaec0ed2cdbf
J. Zebedee <zebedee@code.gripe>
2025-07-25 14:39:33 +0000

    Update chrome versions

--- install_curl.sh
+++ install_curl.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 
 if ! [ -x "$(command -v curl-impersonate)" ]; then
-    CURL_URL="https://github.com/lexiforest/curl-impersonate/releases/download/v1.1.1/curl-impersonate-v1.1.1.x86_64-linux-gnu.tar.gz"
-    CURL_SHA="e5e128c94f9a6423814985ba483bd2fb3ac2c1558441004f672f7db1f1ba15e5"
+    CURL_URL="https://github.com/lexiforest/curl-impersonate/releases/download/v1.1.2/curl-impersonate-v1.1.2.x86_64-linux-gnu.tar.gz"
+    CURL_SHA="439af5886be5033001f1fa38fe5f08409afe160d455b1892dd2f2cc29d018e00"
     CURL_OUT="$(mktemp)"
 
     curl -sL -o "$CURL_OUT" "$CURL_URL"
@@ -12,6 +12,4 @@ if ! [ -x "$(command -v curl-impersonate)" ]; then
     rm "$CURL_OUT"
 fi
 
-sudo cp curl_chrome132 /usr/bin/
-sudo cp curl_chrome136 /usr/bin/
 sudo cp curl_chrome138 /usr/bin/
