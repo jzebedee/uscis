36e111b280d519841486ee8b1843ec4c08cdff88
J. Zebedee <zebedee@code.gripe>
2025-08-04 04:39:43 +0000

    Update to curl-impersonate 1.2.0

--- install_curl.sh
+++ install_curl.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 
 if ! [ -x "$(command -v curl-impersonate)" ]; then
-    CURL_URL="https://github.com/lexiforest/curl-impersonate/releases/download/v1.1.2/curl-impersonate-v1.1.2.x86_64-linux-gnu.tar.gz"
-    CURL_SHA="439af5886be5033001f1fa38fe5f08409afe160d455b1892dd2f2cc29d018e00"
+    CURL_URL="https://github.com/lexiforest/curl-impersonate/releases/download/v1.2.0/curl-impersonate-v1.2.0.x86_64-linux-gnu.tar.gz"
+    CURL_SHA="d5c0b31b14a93e3a41d83bc881c3ed0710a41e7742ab7c19051536547404f2c5"
     CURL_OUT="$(mktemp)"
 
     curl -sL -o "$CURL_OUT" "$CURL_URL"
