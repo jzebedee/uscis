c51b95a0f6e2cc1739c6282b80c5809ec16cbed2
J. Zebedee <zebedee@code.gripe>
2026-04-24 18:43:10 +0000

    Glob the files in batch normalize

--- download_form_types_offices.sh
+++ download_form_types_offices.sh
@@ -3,8 +3,4 @@ set -euxo pipefail
 
 sqlite3 uscis.db < query-form-type-offices-urls.sql | xargs -r -t -L 1 ./curl_request_next_action.sh
 
-shopt -s nullglob
-json_files=(response-form-offices_*.json)
-if ((${#json_files[@]})); then
-  uv run normalize_next_response.py "${json_files[@]}"
-fi
+uv run normalize_next_response.py response-form-offices_*.json
--- download_processing_times.sh
+++ download_processing_times.sh
@@ -3,8 +3,4 @@ set -euxo pipefail
 
 sqlite3 uscis.db < query-processing-time-urls.sql | xargs -r -t -L 1 ./curl_request_next_action.sh
 
-shopt -s nullglob
-json_files=(response-processing-time_*.json)
-if ((${#json_files[@]})); then
-  uv run normalize_next_response.py "${json_files[@]}"
-fi
+uv run normalize_next_response.py response-processing-time_*.json
